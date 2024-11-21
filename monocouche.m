d24f09bea08979c1e8c351f128dc655cea0d3bab
Simon Riou <80064281+simonriou@users.noreply.github.com>
2024-11-21 21:51:35 +0100

    Translated monocouche.m

--- monocouche.m
+++ monocouche.m
@@ -3,31 +3,31 @@ close all
 clc
 
 digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-P = length(digits); % Nb perceptrons = nb de digits
+P = length(digits); % Nb of output neurons = nb of classes
 
-% Menu - choix entraînement ou test de la monocouche
-disp("ATTENTION à bien tester les données sur le bon entraînement !");
-disp('Choisissez une option :');
-disp('1. Entraînement');
+% Menu - choose between training and testing
+disp("BE CAREFUL to test the model on the right weights!");
+disp('Choose an option :');
+disp('1. Training');
 disp('2. Test');
-disp('3. Quitter');
-choice = input('Entrez votre choix (1, 2 ou 3) : ');
+disp('3. Close');
+choice = input('Your choice (1, 2 or 3): ');
 
 if choice == 1
-    disp('Lancement de l''entraînement...');
+    disp('Beginning training...');
     trainModel(digits, P);
 elseif choice == 2
-    disp('Lancement du test...');
+    disp('Beginning testing...');
     testModel(digits, P);
 elseif choice == 3
-    disp('Arrêt du script.');
+    disp('Killing script.');
     return;
 else
-    disp('Choix non valide. Veuillez redémarrer et entrer 1, 2 ou 3.');
+    disp('Invalid choice. Please restart the program and choose between 1, 2 or 3.');
 end
 
 function trainModel(digits, P)
-    %% Initialisation des données
+    %% Data loading
     
     rawImgs = [];
     inputLabels = [];
@@ -35,7 +35,7 @@ function trainModel(digits, P)
     for p = 1:P
         digit = digits(p);
         
-        % Données d'entraînement
+        % Training Data
         trainData = load(sprintf("Data/DigitTest_%d.mat", digit));
         rawImgs = cat(3, rawImgs, trainData.imgs);
         inputLabels = [inputLabels ; trainData.labels];
@@ -43,25 +43,25 @@ function trainModel(digits, P)
     
     inputLabels = inputLabels';
     
-    N = size(rawImgs, 3); % Nb de données d'entrapinement
-    ImgSize = size(rawImgs, 1) * size(rawImgs, 2); % Taille des images
+    N = size(rawImgs, 3); % Size of the training set
+    ImgSize = size(rawImgs, 1) * size(rawImgs, 2); % Images size
     
-    %% Mise en forme des données d'entrée
-    shapedImgs = reshape(rawImgs, ImgSize, N); % Format vecteurs colonnes
+    %% Resizing input images
+    shapedImgs = reshape(rawImgs, ImgSize, N); % Column vectors
     
-    biasesX = ones(1, N); % Des 1 pour le biais
+    biasesX = ones(1, N); % Biases
     shapedImgs = [biasesX ; shapedImgs];
     
-    w = zeros(ImgSize+1, P)*0.01; % Poids initiaux
+    w = zeros(ImgSize+1, P)*0.01; % Initial weights
     
-    %% Paramètres de la descente de gradient
+    %% Gradient descent parameters
     rho = 0.1;
     Nb = 500;
     grad = zeros(Nb, ImgSize+1);
     f = zeros(Nb, 1);
         
-    for p = 1:P % Pour chaque perceptron de la couche
-        % Mise en forme des labels
+    for p = 1:P % For each perceptron
+        % Label formatting
         pLabels = zeros(1, N);
     
         for i = 1:N
@@ -70,44 +70,44 @@ function trainModel(digits, P)
             end
         end
     
-        % Initialisation de la descente
+        % Descent initialization
         Y = 1./(1+exp(-(shapedImgs' * w(:,p))'));
         S = (Y - pLabels).^2;
         f(1) = 1/(2*Nb) * sum(S, 2);
         grad(1,:) = (Y - pLabels).*(Y - Y.^2)*shapedImgs';
     
-        % Descente de gradient
+        % Gradient descent
         for i = 2:Nb
             Y = 1./(1+exp(-(shapedImgs' * w(:,p))'));
             S = (Y - pLabels).^2;
-            f(i) = 1/(2*Nb) * sum(S, 2); % Mise à jour du critère
+            f(i) = 1/(2*Nb) * sum(S, 2); % Loss update
     
-            grad(i,:) = shapedImgs * ((Y - pLabels).*Y.*(1-Y)).'/Nb; % Mise à jour du gradient
+            grad(i,:) = shapedImgs * ((Y - pLabels).*Y.*(1-Y)).'/Nb; % Gradient update
     
-            w(:,p) = w(:,p) - rho * grad(i, :)'; % Mise à jour des poids
+            w(:,p) = w(:,p) - rho * grad(i, :)'; % Weight update
         end
         
         save('modele_perceptron.mat', 'w');
     end
-    disp("Réseau entraîné.");
+    disp("Model successfully trained.");
 end
 
 function testModel(digits, P)
-    %% Initialiation des données de test
+    %% Test data loading
     testImgs = [];
     testLabels = [];
     
     for p = 1:P
         digit = digits(p);
        
-        % Données de test
+        % Test set
         testData = load(sprintf("Data/DigitTest_%d.mat", digit));
         testImgs = cat(3, testImgs, testData.imgs);
         testLabels = [testLabels ; testData.labels];
     end
     testLabels = testLabels';
-    NTest = size(testImgs, 3); % Nb de données de test
-    ImgSize = size(testImgs, 1) * size(testImgs, 2); % Taille des images
+    NTest = size(testImgs, 3); % Test data size
+    ImgSize = size(testImgs, 1) * size(testImgs, 2); % Images size
     
     shapedTestImgs = reshape(testImgs, ImgSize, NTest);
     biasesXTest = ones(1, NTest);
@@ -116,7 +116,7 @@ function testModel(digits, P)
     wData = load("modele_perceptron.mat");
     w = wData.w;
 
-    %% Calcul de la sortie de la couche
+    %% Ouput computation
     outputTestLabels = zeros(P, NTest);
     
     for p = 1:P
@@ -125,25 +125,25 @@ function testModel(digits, P)
         outputTestLabels(p, :) = YP';
     end
     
-    % Attribution des classes de sortie
-    for i = 1:NTest % Pour chaque jeu de données
-        [~, imax] = max(outputTestLabels(:,i)); % On trouve l'indice de la plus grande valeur
-        outputTestLabels(:, i) = 0; % On met la colonne à 0
-        outputTestLabels(imax, i) = 1; % On met la plus grande à 1
+    % Class labelization
+    for i = 1:NTest % For each data set
+        [~, imax] = max(outputTestLabels(:,i)); % Get the indice of the greatest
+        outputTestLabels(:, i) = 0; % Set the column to 0
+        outputTestLabels(imax, i) = 1; % And set the greatest to 1
     end
     
     finalLabels = zeros(1, NTest);
     
-    % On remet en forme les labels avec les digits
+    % Label formatting
     for i = 1:NTest
         idx = outputTestLabels(:,i) == 1;
         finalLabels(1, i) = digits(idx);
     end
     
-    %% Matrice de confusion
+    %% Confusion matrix
     confM = confusionmat(testLabels, finalLabels);
-    f3 = figure('Name', 'Matrice de confusion');
+    f3 = figure('Name', 'Confusion matrix');
     confusionchart(confM)
 
-    disp("Test terminé.");
+    disp("Test done.");
 end
